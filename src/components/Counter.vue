d6087425affa9f22a8344269e4b41f65485240d4
lixingyang <1433565248@qq.com>
2021-07-08 11:27:50 +0800

    add useCounter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,13 +1,14 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="handleClick">counter is: {{ data.counter }}</button>
-  <p>{{data.counter}}</p>
-  <p>{{data.doubleCounter}}</p>
+  <button @click="handleClick">counter is: {{ counter }}</button>
+  <p>{{counter}}</p>
+  <p>{{doubleCounter}}</p>
   <p>{{msg2}}</p>
+  <p ref="desc"></p>
 </template>
 
 <script>
-import { reactive, onMounted, computed, onUnmounted, ref } from 'vue'
+import { reactive, onMounted, computed, onUnmounted, ref, toRefs, watch } from 'vue'
 
 export default {
   name: 'Counter',
@@ -16,27 +17,38 @@ export default {
   },
   handleClick() {
     console.log('bug')
-    data.counter++
+    counter++
   },
   setup() {
-    const data = reactive({
-      counter: 1,
-      doubleCounter: computed(() => data.counter * 2)
-    })
-    let timer 
-    onMounted(() => {
-      timer = setInterval(() => {
-        data.counter++ 
-      }, 1000);
-    })
-    onUnmounted(() => {
-      clearInterval(timer)
-    })
+    const { counter, doubleCounter } = useCounter()
     const msg2 = ref("some message")
+    const desc = ref(null)
+    watch(counter, (val, oldVal) => {
+      const p = desc.value
+      p.textContext = `counter change from ${oldVal} to ${val}`
+    })
     return {
-      data,
-      msg2
+      counter, 
+      doubleCounter,
+      msg2,
+      desc
     }
   }
+}
+function useCounter() {
+  const data = reactive({
+    counter: 1,
+    doubleCounter: computed(() => data.counter * 2)
+  })
+  let timer 
+  onMounted(() => {
+    timer = setInterval(() => {
+      data.counter++ 
+    }, 1000);
+  })
+  onUnmounted(() => {
+    clearInterval(timer)
+  })
+  return toRefs(data)
 }
 </script>
